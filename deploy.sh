cc89f12a78f6880f5de6b1db4e6c513efe48cf17
David Huculak <davidhuculak5@gmail.com>
2019-05-17 05:21:39 -0400

    changed deploy script to remove all remote files before copying new ones

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,14 @@
 
 REMOTE="ec2c"
 REMOTE_GRAV_FOLDER="/var/www/html/"
-REMOTE_ZIP_NAME="temp.zip"
+REMOTE_TEMP_FILE_PATH="/tmp/temp.zip"
 REMOTE_PERFMIX_NAME="permfixUbuntu.sh"
-REMOTE_TEMP_FILE_PATH="$REMOTE_GRAV_FOLDER$REMOTE_ZIP_NAME"
 
 cd grav
 sudo zip -rq temp.zip . -x cache/\* logs/\*
 scp ./temp.zip $REMOTE:$REMOTE_TEMP_FILE_PATH
+ssh -t $REMOTE "rm -rf $REMOTE_GRAV_FOLDER*"
+ssh -t $REMOTE "mkdir "$REMOTE_GRAV_FOLDER"logs "$REMOTE_GRAV_FOLDER"cache"
 ssh -t $REMOTE "unzip -oq $REMOTE_TEMP_FILE_PATH -d $REMOTE_GRAV_FOLDER"
 ssh -t $REMOTE "cd $REMOTE_GRAV_FOLDER && sudo ./$REMOTE_PERFMIX_NAME"
 ssh -t $REMOTE "rm $REMOTE_TEMP_FILE_PATH"
